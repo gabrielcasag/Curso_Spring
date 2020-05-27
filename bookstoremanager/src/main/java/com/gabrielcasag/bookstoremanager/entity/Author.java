3ad0976385918b47c1d6adfc88b5c752b16681ae
Gabriel Casagrande <gaspericasag@gmail.com>
2020-05-26 22:42:47 -0300

    Entidades mapeadas

---
+++ bookstoremanager/src/main/java/com/gabrielcasag/bookstoremanager/entity/Author.java
@@ -0,0 +1,44 @@
+package com.gabrielcasag.bookstoremanager.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false , unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    public Author() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
