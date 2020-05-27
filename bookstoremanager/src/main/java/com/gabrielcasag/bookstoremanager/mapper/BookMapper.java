3e552f02d5fc4503e024713cd7a6125292e32b76
Gabriel Casagrande <gaspericasag@gmail.com>
2020-05-27 19:09:32 -0300

    Adicionado Mapstruct ao projeto

---
+++ bookstoremanager/src/main/java/com/gabrielcasag/bookstoremanager/mapper/BookMapper.java
@@ -0,0 +1,16 @@
+package com.gabrielcasag.bookstoremanager.mapper;
+
+import com.gabrielcasag.bookstoremanager.dto.BookDTO;
+import com.gabrielcasag.bookstoremanager.entity.Book;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BookMapper {
+
+    BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
+
+    Book toModel(BookDTO bookDTO);
+
+    BookDTO toDTO(Book book);
+}
--- bookstoremanager/src/main/java/com/gabrielcasag/bookstoremanager/service/BookService.java
+++ bookstoremanager/src/main/java/com/gabrielcasag/bookstoremanager/service/BookService.java
@@ -3,6 +3,7 @@ package com.gabrielcasag.bookstoremanager.service;
 import com.gabrielcasag.bookstoremanager.dto.BookDTO;
 import com.gabrielcasag.bookstoremanager.dto.MessageResponseDTO;
 import com.gabrielcasag.bookstoremanager.entity.Book;
+import com.gabrielcasag.bookstoremanager.mapper.BookMapper;
 import com.gabrielcasag.bookstoremanager.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,14 @@ public class BookService {
 
     private BookRepository bookRepository;
 
+    private final BookMapper bookMapper = BookMapper.INSTANCE;
     @Autowired
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
     public MessageResponseDTO create(BookDTO bookDTO) {
-        Book bookToSave = Book.builder().build();
+        Book bookToSave = bookMapper.toModel(bookDTO);
 
         Book savedBook = bookRepository.save(bookToSave);
 
